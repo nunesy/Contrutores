d912775c7c2f7ba0a2b9dec0353a42ed8115063c
nunesy <yurimormon@gmail.com>
2022-01-07 23:06:33 -0300

    Exemplo Básico de Construtor Privado

---
+++ ExemploConstrutores/Models/Log.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploConstrutores.Models
+{
+    public class Log
+    {
+
+        private static Log _log;
+
+        public string PropriedadeLog { get; set; }
+        private Log()
+        {
+            
+        }
+
+        public static Log GetInstance()
+        {
+            if (_log == null)
+            {
+                _log = new Log();
+            }
+            return _log;
+        }
+    }
+}
--- ExemploConstrutores/Program.cs
+++ ExemploConstrutores/Program.cs
@@ -7,8 +7,17 @@ namespace ExemploConstrutores
     {
         static void Main(string[] args)
         {
-            Pessoa p1 = new Pessoa();
-            p1.Apresentar();
+
+            Log log = Log.GetInstance();
+
+            log.PropriedadeLog = "Teste instância";
+
+            Log log2 = Log.GetInstance();
+            System.Console.WriteLine(log2.PropriedadeLog);
+
+
+            //Pessoa p1 = new Pessoa();
+            //p1.Apresentar();
         }
     }
 }
